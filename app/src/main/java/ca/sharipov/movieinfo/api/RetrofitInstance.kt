fe44f520f50e127fbeb7cb73e5a242475eb6d63b
SergeySharipov <sharipov14@gmail.com>
2020-12-26 23:08:54 -0500

    Add cache for network responses

--- app/src/main/java/ca/sharipov/movieinfo/api/RetrofitInstance.kt
+++ app/src/main/java/ca/sharipov/movieinfo/api/RetrofitInstance.kt
@@ -1,18 +1,26 @@
 package ca.sharipov.movieinfo.api
 
+import android.os.Environment
 import ca.sharipov.movieinfo.util.Constants.Companion.BASE_URL
+import okhttp3.Cache
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+
 class RetrofitInstance {
     companion object {
 
         private val retrofit by lazy {
+            val cacheSize: Long = 20 * 1024 * 1024 // 20 MB
+            val cache = Cache(Environment.getDownloadCacheDirectory(), cacheSize)
+
             val logging = HttpLoggingInterceptor()
             logging.setLevel(HttpLoggingInterceptor.Level.BASIC)
+
             val client = OkHttpClient.Builder()
+                .cache(cache)
                 .addInterceptor(RequestInterceptor())
                 .addInterceptor(logging)
                 .build()
