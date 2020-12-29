8b0a60a6e8f2c36d934a4786a59508dd9f89126e
SergeySharipov <sharipov14@gmail.com>
2020-12-29 13:27:24 -0500

    Update FakeMoviesRepository.kt

--- app/src/test/java/ca/sharipov/movieinfo/repositories/FakeMoviesRepository.kt
+++ app/src/test/java/ca/sharipov/movieinfo/repositories/FakeMoviesRepository.kt
@@ -10,6 +10,15 @@ import retrofit2.Response
 
 class FakeMoviesRepository : MoviesRepository {
 
+    private val movieBriefItems = mutableListOf<MovieBrief>()
+
+    private val observableMovieBriefItems = MutableLiveData<List<MovieBrief>>(movieBriefItems)
+    private val observableMovieBrief = MutableLiveData<MovieBrief>()
+
+    private fun refreshLiveData() {
+        observableMovieBriefItems.postValue(movieBriefItems)
+    }
+
     override suspend fun getMovie(movieId: Int): Response<Movie> {
         TODO("Not yet implemented")
     }
@@ -30,18 +39,22 @@ class FakeMoviesRepository : MoviesRepository {
     }
 
     override suspend fun upsert(movieBrief: MovieBrief): Long {
-        TODO("Not yet implemented")
-    }
-
-    override fun getAllMovieBriefs(): LiveData<List<MovieBrief>> {
-        TODO("Not yet implemented")
+        movieBriefItems.add(movieBrief)
+        refreshLiveData()
+        return 0
     }
 
     override suspend fun deleteMovieBrief(movieBrief: MovieBrief) {
-        TODO("Not yet implemented")
+        movieBriefItems.remove(movieBrief)
+        refreshLiveData()
+    }
+
+    override fun getAllMovieBriefs(): LiveData<List<MovieBrief>> {
+        return observableMovieBriefItems
     }
 
     override fun getMovieBrief(id: Int): LiveData<MovieBrief> {
-        TODO("Not yet implemented")
+        return observableMovieBrief
     }
 }
+
