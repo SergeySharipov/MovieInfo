3288d688848ae41004e9f3f411cb2b96e9a1d701
SergeySharipov <sharipov14@gmail.com>
2022-01-20 12:02:11 -0500

    Update to use https

--- app/src/main/java/ca/sharipov/serhii/movieinfo/utils/Constants.kt
+++ app/src/main/java/ca/sharipov/serhii/movieinfo/utils/Constants.kt
@@ -4,9 +4,9 @@ class Constants {
     companion object {
         const val DATABASE_NAME = "movies.db"
 
-        const val BASE_URL = "http://api.themoviedb.org/"
-        const val POSTER_URL = "http://image.tmdb.org/t/p/w342"
-        const val BACKDROP_URL = "http://image.tmdb.org/t/p/w780"
+        const val BASE_URL = "https://api.themoviedb.org/"
+        const val POSTER_URL = "https://image.tmdb.org/t/p/w342"
+        const val BACKDROP_URL = "https://image.tmdb.org/t/p/w780"
 
         const val QUERY_PAGE_SIZE = 19
     }
